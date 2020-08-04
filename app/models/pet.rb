72d126c60eae4d63493c25e9f64a25dd0d902ff4
Bennis Yiu <bennisyiu@gmail.com>
2020-08-04 13:24:08 +0800

    validations for 3 models added, user login controller created but not completed

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,9 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :gender inclusion: { in: ['Male', 'Female'] }
+  validates :neutered, default: false, null: false
+  validates :description, length: { minimum: 20 }
+  validates :name, :breed, :neutered, :price, :age, :description, presence: true
 end
