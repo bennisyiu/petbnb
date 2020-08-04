40805f86e74c010b11113e848c1e9345cf7b32ed
Bennis Yiu <bennisyiu@gmail.com>
2020-08-04 12:27:54 +0800

    user open id changed to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_104632) do
+ActiveRecord::Schema.define(version: 2020_08_04_042218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_104632) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "phone"
-    t.integer "open_id"
+    t.string "open_id"
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
