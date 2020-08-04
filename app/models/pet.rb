cc914843c9d679984335b4129661e28320830b7e
Bennis Yiu <bennisyiu@gmail.com>
2020-08-04 16:38:05 +0800

    skip verify auth token

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,8 +2,8 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :gender inclusion: { in: ['Male', 'Female'] }
+  validates :gender, inclusion: { in: ['Male', 'Female'] }
   validates :neutered, default: false, null: false
-  validates :description, length: { minimum: 20 }
+  validates :description, length: { minimum: 10 }
   validates :name, :breed, :neutered, :price, :age, :description, presence: true
 end
