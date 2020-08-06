61408e6a7e5f5ceb6d25759261d19345f50892cf
Bennis Yiu <bennisyiu@gmail.com>
2020-08-06 09:40:36 +0800

    routes and controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :pets, only: [ :index, :show, :create, :destroy ]
+      resources :pets, only: [:index, :show, :create, :destroy] do
+        resources :bookings, only: [:index, :show, :create]
+      end
+      reources :bookings, only: [:destroy]
     end
   end
 end
